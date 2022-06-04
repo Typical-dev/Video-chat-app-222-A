9ad7d85a88e462b19583acad69390e8c361fb2a2
Typical-dev <70384457+Typical-dev@users.noreply.github.com>
2022-06-04 17:38:09 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-        user: 'abhishekdodrai@gmail.com',
-        pass: 'vtnqvrclpytkllzq',
+        user: '',
+        pass: '',
     },
     secure: true,
 });
